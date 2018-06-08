4bba0973afdced07ab2c05715a9793dc145dbe0a
Sina <sina6002@gmail.com>
2018-06-08 17:15:38 +0100

    Ignore attributes potentially containing JSON/HTML
    Some websites have the habit of putting HTML/JSON in data attributes, causing the unique-selector builder to generate invalid querySelectors. This commit adds a fairly naive check to skip these attributes.

--- src/getAttributes.js
+++ src/getAttributes.js
@@ -11,7 +11,8 @@ export function getAttributes( el, attributesToIgnore = ['id', 'class', 'length'
 
   return attrs.reduce( ( sum, next ) =>
   {
-    if ( ! ( attributesToIgnore.indexOf( next.nodeName ) > -1 ) )
+    if ( ! ( attributesToIgnore.indexOf( next.nodeName ) > -1 ) 
+        && !(next.value && (next.value.indexOf('{') > -1 || next.value.indexOf('<') > -1)))
     {
       sum.push( `[${next.nodeName}="${next.value}"]` );
     }
