437257d10130fb0443104bcf3bc70deb8a414858
Manuel Abascal <manuelsuarezabascal@gmail.com>
2021-11-15 02:27:50 -0500

    issue-1884: address PR comment

--- src/getData.js
+++ src/getData.js
@@ -1,16 +1,26 @@
+import 'css.escape';
+
 /**
  * Returns the data-{dataAttr} selector of the element wrapped in double quotes.
- * @param  { String } selectorType
- * @return { Function }
- * @param  { Object } element
- * @return { String }
+ * @param  { String } selectorType - The type of selector to return.
+ * @return { String } - The data-{dataAttr} selector of the element wrapped in double quotes.
  */
+
 export const getData = ( selectorType, attributes ) => {
   for ( let i = 0; i < attributes.length; i++ ) {
-    const { nodeName, value } = attributes[ i ]
+    // extract node name + value
+    const { nodeName, value } = attributes[ i ];
 
-    if ( nodeName === selectorType ) return `[${nodeName}="${value}"]`
+    // if this matches our selector
+    if ( nodeName === selectorType ){
+      if ( value ) {
+      // if we have value that needs quotes
+      return `[${nodeName}="${value}"]`;
+      }
+
+      return `[${nodeName}]`;
+    }
   }
-
-  return null
-}
+ 
+   return null;
+ };
