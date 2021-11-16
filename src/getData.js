9bb59a1511b53907610bcf236e541b4e7a010df7
Chris Breiding <chrisbreiding@gmail.com>
2021-11-16 10:13:37 -0500

    formatting changes

--- src/getData.js
+++ src/getData.js
@@ -1,24 +1,29 @@
 /**
- * Returns the data-{dataAttr} selector of the element wrapped in double quotes.
+ * Returns the data-{dataAttr} selector of the element
  * @param  { String } selectorType - The type of selector to return.
- * @return { String } - The data-{dataAttr} selector of the element wrapped in double quotes.
+ * @param  { String } attributes - The attributes of the element.
+ * @return { String | null } - The data-{dataAttr} selector of the element.
  */
 
-export const getData = ( selectorType, attributes ) => {
-  for ( let i = 0; i < attributes.length; i++ ) {
+export const getData = ( selectorType, attributes ) =>
+{
+  for ( let i = 0; i < attributes.length; i++ )
+  {
     // extract node name + value
     const { nodeName, value } = attributes[ i ];
 
     // if this matches our selector
-    if ( nodeName === selectorType ){
-      if ( value ) {
-      // if we have value that needs quotes
-      return `[${nodeName}="${value}"]`;
+    if ( nodeName === selectorType )
+    {
+      if ( value )
+      {
+        // if we have value that needs quotes
+        return `[${nodeName}="${value}"]`;
       }
 
       return `[${nodeName}]`;
     }
   }
- 
-   return null;
- };
+
+  return null;
+};
